1d83892d1a2be44f9877381003f6e822f2dbbeaf
Luke Kavanagh <lukekavanagh1@yahoo.co.uk>
2016-02-06 18:17:45 +1300

    initial commit

---
+++ Models/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoBooks.Models
+{
+    public class Book
+    {
+        [ScaffoldColumn(false)]
+        public int BookId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public int Year { get; set; }   
+        [Range(1, 500)]
+        public decimal Price { get; set; }
+
+        public string Genre { get; set; }
+        [ScaffoldColumn(false)]
+        public int AuthorId { get; set; }
+
+        //Navigation Property
+        public virtual Author Author { get; set; }
+    }
+}
---
+++ Models/SampleData.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+
+namespace ContosoBooks.Models
+{
+    public static class SampleData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            var context = serviceProvider.GetService<ApplicationDbContext>();
+            context.Database.Migrate();
+            if (context.Book.Any())
+            {
+                var whitman = context.Author.Add(
+                    new Author {LastName = "Whitman", FirstMidName = "Walt"}).Entity;
+                var dickens = context.Author.Add(
+                    new Author {LastName = "Dickens", FirstMidName = "Charles"}).Entity;
+                var thompson = context.Author.Add(
+                    new Author {LastName = "Thompson", FirstMidName = "Hunter S"}).Entity;
+
+                context.Book.AddRange(
+                    new Book()
+                    {
+                        Title = "Leaves of Grass",
+                        Year = 1813,
+                        Author = whitman,
+                        Price = 9.99m,
+                        Genre = "Poems"
+                    },
+                    new Book()
+                    {
+                        Title = "Song of Myself",
+                        Year = 1817,
+                        Author = whitman,
+                        Price = 15,
+                        Genre = "Poems"
+                    },
+                    new Book()
+                    {
+                        Title = "Fear and loathing in Las Vegas",
+                        Year = 1963,
+                        Author = thompson,
+                        Price = 5.63M,
+                        Genre = "The fallacy of the American dream"
+                    },
+                    new Book()
+                    {
+                        Title = "David Copperfield",
+                        Year = 1850,
+                        Author = dickens,
+                        Price = 16,
+                        Genre = "Victorian"
+                    },
+                    new Book()
+                    {
+                        Title = "Hell's Angels: The Strange and Terrible Saga of the Outlaw Motorcycle Gangs",
+                        Year = 1969,
+                        Author = thompson,
+                        Price = 19.69M,
+                        Genre = "Psychedelia"
+                    },
+                    new Book()
+                    {
+                        Title = "Great Expectations",
+                        Year = 1843,
+                        Author = dickens,
+                        Price = 18.54M,
+                        Genre = "Victorian"
+                    }
+                 );
+                context.SaveChanges();    
+                }
+            }
+        }
+    }
